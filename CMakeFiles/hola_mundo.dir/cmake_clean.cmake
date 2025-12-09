ce1c100686b811500fbda7ce191174e2b00453cf
Miguel Lacruz <malacruz132@gmail.com>
2025-12-09 07:55:56 -0500

    HOla mundo

---
+++ CMakeFiles/hola_mundo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hola_mundo.dir/link.d"
+  "CMakeFiles/hola_mundo.dir/main.c.o"
+  "CMakeFiles/hola_mundo.dir/main.c.o.d"
+  "hola_mundo"
+  "hola_mundo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hola_mundo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
